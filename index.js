e7fc21936c3d2744f44c6228f0514b1ab2295e43
Kevin <mrhallonline@gmail.com>
2019-10-16 23:03:55 -0500

    kinda works consolelogs the outcome

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 
 var randomNumber;
-var shoot;
 var computerSelection;
-computerPlay();
+var playerSelection;
 var levelCounter = 0;
 
-function startGame(){
-    playRound();
-}
+computerPlay();
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     levelCounter++;
-    playerChoose();
-    computerPlay();
+    
+    if (playerSelection == computerSelection){
+        console.log("It's a draw " + playerSelection + " vs " + computerSelection);
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("You lost " + playerSelection + " loses to " + computerSelection);
+        youLost();
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("You won " + playerSelection + " beats " + computerSelection);
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("You lost " + playerSelection + " loses to " + computerSelection);
+        youLost();
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("You won " + playerSelection + " beats " + computerSelection);
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("You lost " + playerSelection + " loses to " + computerSelection);
+        youLost();
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("You won " + playerSelection + " beats " + computerSelection);
+    }
 }
 
-function round(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-            console.log("It's a draw " + playerSelection + " vs " + computerSelection);
-    }
-    if (playerSelection === "rock" && computerSelection === "paper");{
-        console.log("You lost " + playerSelection + " loses to " + computerSelection);
-        youLost();
-    }
-    if (playerSelection === "rock" && computerSelection === "scissors");{
-        console.log("You won " + playerSelection + " beats " + computerSelection);
-    }
-    if (playerSelection === "paper" && computerSelection === "scissors");{
-        console.log("You lost " + playerSelection + " loses to " + computerSelection);
-        youLost();
-    }
-    if (playerSelection === "paper" && computerSelection === "rock");{
-        console.log("You won " + playerSelection + " beats " + computerSelection);
-    }
-    if (playerSelection === "scissors" && computerSelection === "rock");{
-        console.log("You lost " + playerSelection + " vs " + computerSelection);
-        youLost();
-    }
-    if (playerSelection === "scissors" && computerSelection === "paper");{
-        console.log("You won " + playerSelection + " vs " + computerSelection);
-    }
-}
 
 function computerPlay() {
     randomNumber = Math.floor(Math.random() * 3);
@@ -53,24 +47,29 @@ function computerPlay() {
     if (randomNumber === 2) {
         computerSelection = "scissors";
     }
-    console.log(computerSelection);
 }
 
-function playerChoose () {
+
     $("#rock").click(function () {
         playerSelection = "rock";
-        console.log(playerSelection);
+        playRound();
+       
+        // console.log(playerSelection);
     });
     
     $("#paper").click(function () {
         playerSelection = "paper";
-        console.log(playerSelection);
+        playRound();
+        
+        // console.log(playerSelection);
     });
     $("#scissors").click(function () {
         playerSelection = "scissors";
-        console.log(playerSelection);
+        playRound();
+   
+        // console.log(playerSelection);
     });
-}
+
 
 function youLost(){
     level = 0;
