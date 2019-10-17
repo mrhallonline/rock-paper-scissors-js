1ff8e9c2b1dc702e8b969d3bae7d133d3210cfda
Kevin <mrhallonline@gmail.com>
2019-10-16 22:10:33 -0500

    set up round function player vs computer

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-// function playRound(playerSelection, computerSelection) {
-// 	// your code here!
-// }
-
-// const playerSelection = 'rock'
-// const computerSelection = computerPlay()
-// console.log(playRound(playerSelection, computerSelection))
-
-
 
 var randomNumber;
 var shoot;
 var computerSelection;
 computerPlay();
+var levelCounter = 0;
+
+function startGame(){
+    playRound();
+}
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return ("Draw");
+    levelCounter++;
+    playerChoose();
+    computerPlay();
+}
+
+function round(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+            console.log("It's a draw " + playerSelection + " vs " + computerSelection);
+    }
+    if (playerSelection === "rock" && computerSelection === "paper");{
+        console.log("You lost " + playerSelection + " loses to " + computerSelection);
+        youLost();
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors");{
+        console.log("You won " + playerSelection + " beats " + computerSelection);
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors");{
+        console.log("You lost " + playerSelection + " loses to " + computerSelection);
+        youLost();
+    }
+    if (playerSelection === "paper" && computerSelection === "rock");{
+        console.log("You won " + playerSelection + " beats " + computerSelection);
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock");{
+        console.log("You lost " + playerSelection + " vs " + computerSelection);
+        youLost();
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper");{
+        console.log("You won " + playerSelection + " vs " + computerSelection);
     }
 }
 
@@ -33,8 +56,6 @@ function computerPlay() {
     console.log(computerSelection);
 }
 
-
-
 function playerChoose () {
     $("#rock").click(function () {
         playerSelection = "rock";
@@ -49,4 +70,10 @@ function playerChoose () {
         playerSelection = "scissors";
         console.log(playerSelection);
     });
+}
+
+function youLost(){
+    level = 0;
+    started = "no";
+
 }
